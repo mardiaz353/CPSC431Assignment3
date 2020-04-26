b007dece200552e4194bcbf1d6251e6b329e2a8f
mauriciomac92 <36291153+mauriciomac92@users.noreply.github.com>
2020-04-26 15:25:10 -0700

    got username and color and sent them to db

--- chat.php
+++ chat.php
@@ -1,88 +1,77 @@
 <?php
-//if(isset($_GET["username"])){
-	session_start();
-	ob_start();
-	header("Content-type: application/json");
-	date_default_timezone_set('UTC');
-	//connect to database
-	$db = mysqli_connect('mariadb', 'cs431s28', 'Moh3poox', 'cs431s28');
-	if (mysqli_connect_errno()) {
-	   echo '<p>Error: Could not connect to database.<br/>
-	   Please try again later.</p>';
-	   exit;
-	}
-	//helper funtion to replace get_results() if without mysqlnd 
-	function get_result( $Statement ) {
-		$RESULT = array();
-		$Statement->store_result();
-		for ( $i = 0; $i < $Statement->num_rows; $i++ ) {
-			$Metadata = $Statement->result_metadata();
-			$PARAMS = array();
-			while ( $Field = $Metadata->fetch_field() ) {
-				$PARAMS[] = &$RESULT[ $i ][ $Field->name ];
-			}
-			call_user_func_array( array( $Statement, 'bind_result' ), $PARAMS );
-			$Statement->fetch();
-		}
-		return $RESULT;
-	}
-	try {
-		//echo "Made it here";
-		$username=$_GET["username"];
-		$insertQuery = "INSERT INTO chatlog 
-		(username) 
-		VALUES ('$username')";
-    
-    
-    // if query works tell user
-		if($db->query($insertQuery)==TRUE) {
-			echo "new record created successfully";
-		}
-	
-		$currentTime = time();
-		$session_id = session_id();    
-		$lastPoll = isset($_SESSION['last_poll']) ? $_SESSION['last_poll'] : $currentTime;    
-		$action = isset($_SERVER['REQUEST_METHOD']) && ($_SERVER['REQUEST_METHOD'] == 'POST') ? 'send' : 'poll';
-		switch($action) {
-			case 'poll':
-			   $query = "SELECT * FROM chatlog WHERE date_created >= ".$lastPoll;
-			   $stmt = $db->prepare($query);
-			   $stmt->execute();
-			   $stmt->bind_result($id, $message, $session_id, $date_created);
-			   $result = get_result( $stmt);
-			   $newChats = [];
-			   while($chat = array_shift($result)) {
-				   
-				   if($session_id == $chat['sent_by']) {
-					  $chat['sent_by'] = 'self';
-				   } else {
-					  $chat['sent_by'] = 'other';
-				   }
-				 
-				   $newChats[] = $chat;
-				}
-			   $_SESSION['last_poll'] = $currentTime;
+session_start();
+ob_start();
+header("Content-type: application/json");
+date_default_timezone_set('UTC');
+//connect to database
+$db = mysqli_connect('localhost', 'root', '', 'chat');
+if (mysqli_connect_errno()) {
+   echo '<p>Error: Could not connect to database.<br/>
+   Please try again later.</p>';
+   exit;
+}
 
-			   print json_encode([
-					'success' => true,
-			'messages' => $newChats
-			   ]);
-			   exit;
-			case 'send':
-				$message = isset($_POST['message']) ? $_POST['message'] : '';            
-				$message = strip_tags($message);
-				$query = "INSERT INTO chatlog (message, sent_by, date_created) VALUES(?, ?, ?)";
-				$stmt = $db->prepare($query);
-				$stmt->bind_param('ssi', $message, $session_id, $currentTime); 
-				$stmt->execute(); 
-				print json_encode(['success' => true]);
-				exit;
-		}
-	} catch(Exception $e) {
-		print json_encode([
-			'success' => false,
-			'error' => $e->getMessage()
-		]);
-	}
-//}
+
+//helper funtion to replace get_results() if without mysqlnd 
+function get_result( $Statement ) {
+    $RESULT = array();
+    $Statement->store_result();
+    for ( $i = 0; $i < $Statement->num_rows; $i++ ) {
+        $Metadata = $Statement->result_metadata();
+        $PARAMS = array();
+        while ( $Field = $Metadata->fetch_field() ) {
+            $PARAMS[] = &$RESULT[ $i ][ $Field->name ];
+        }
+        call_user_func_array( array( $Statement, 'bind_result' ), $PARAMS );
+        $Statement->fetch();
+    }
+    return $RESULT;
+}
+
+try { 
+    $currentTime = time();
+    $session_id = session_id();    
+    $lastPoll = isset($_SESSION['last_poll']) ? $_SESSION['last_poll'] : $currentTime;    
+    $action = isset($_SERVER['REQUEST_METHOD']) && ($_SERVER['REQUEST_METHOD'] == 'POST') ? 'send' : 'poll';
+    switch($action) {
+        case 'poll':
+           $query = "SELECT * FROM chatlog WHERE date_created >= ".$lastPoll;
+           $stmt = $db->prepare($query);
+           $stmt->execute();
+           $stmt->bind_result($id, $message, $session_id, $date_created, $username, $color);
+           $result = get_result( $stmt);
+           $newChats = [];
+           while($chat = array_shift($result)) {
+               if($session_id == $chat['sent_by']) {
+                  $chat['sent_by'] = 'self';
+               } else {
+                  $chat['sent_by'] = 'other';
+               }
+               $newChats[] = $chat;
+            }
+           $_SESSION['last_poll'] = $currentTime;
+
+           print json_encode([
+                'success' => true,
+		'messages' => $newChats
+           ]);
+           exit;
+        case 'send':
+            $message = isset($_POST['message']) ? $_POST['message'] : '';
+            $username = isset($_POST['username']) ? $_POST['username'] : '';
+            $color = isset($_POST['color']) ? $_POST['color'] : '';
+            $message = strip_tags($message);
+            $query = "INSERT INTO chatlog (message, sent_by, date_created, username, color) VALUES(?, ?, ?, ?, ?)";
+            $stmt = $db->prepare($query);
+            $stmt->bind_param('ssiss', $message, $session_id, $currentTime, $username, $color); 
+            $stmt->execute(); 
+            print json_encode(['success' => true]);
+            exit;
+    }
+} catch(Exception $e) {
+    print json_encode([
+        'success' => false,
+        'error' => $e->getMessage()
+    ]);
+}
 ?>
